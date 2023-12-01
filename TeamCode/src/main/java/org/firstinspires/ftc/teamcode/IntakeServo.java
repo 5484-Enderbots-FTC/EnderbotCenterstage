f6ee784abfffcde989aa55ba855641b227887215
SiEclipzi <110932027+SiEclipzi@users.noreply.github.com>
2023-11-30 19:41:12 -0500

    test servo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakeServo.java
@@ -1,19 +1,7 @@
 package org.firstinspires.ftc.teamcode;
-
-import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
-
-import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad2;
-
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
-
-import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 //
@@ -21,22 +9,26 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 @TeleOp(name = "IntakeServo", group = "Linear Opmode")
 
 public class IntakeServo extends LinearOpMode {
-    SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
     Servo intakeLeft;
-   // Servo IntakeRight;
+    Servo intakeRight;
 
     @Override
     public void runOpMode() throws InterruptedException {
         intakeLeft = hardwareMap.get(Servo.class, "leftSvr");
+        intakeRight = hardwareMap.get(Servo.class, "rightSvr");
 
-        if (gamepad1.a) {
-            intakeLeft.setPosition(0.5 + (gamepad1.left_stick_y * 0.1));
-          //  IntakeRight.setPosition(.9);
-        }  else if (!gamepad1.a){
-            intakeLeft.setPosition(0.5 - (gamepad1.left_stick_y * 0.1));
-          //  IntakeRight.setPosition(0.2);
-        }
+        waitForStart();
 
+        while (opModeIsActive())
+
+            if (!gamepad1.a) {
+                intakeLeft.setPosition(intakeLeft.getPosition() + (gamepad1.left_stick_y * 0.1));
+                //intakeRight.setPosition(intakeRight.getPosition() + (gamepad1.right_stick_y * 0.1));
+            }  else if (gamepad1.a){
+                intakeLeft.setPosition(intakeLeft.getPosition() - (gamepad1.left_stick_y * 0.1));
+                //intakeRight.setPosition(intakeRight.getPosition() - (gamepad1.right_stick_y * 0.1));
+            }
+        telemetry.addData("left servo", "pos: " + intakeLeft.getPosition());
     }
 }
