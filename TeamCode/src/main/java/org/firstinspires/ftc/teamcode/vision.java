f1981b09da51ad39e487d32ea72d270f9426607f
KJScodes <104269100+KJScodes@users.noreply.github.com>
2023-11-30 19:32:13 -0500

    lol

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision.java
@@ -4,11 +4,17 @@ import android.graphics.Canvas;
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Mat;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Mat;
+import org.opencv.core.Rect;
 
-public class vision implements VisionProcessor {
+
+    public class vision implements VisionProcessor {
+    public Rect rect = new Rect(20, 20, 50, 50);
+
     @Override
     public void init(int width, int height, CameraCalibration calibration) {
-
     }
 
     @Override
@@ -16,10 +22,29 @@ public class vision implements VisionProcessor {
         return null;
     }
 
-    @Override
-    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight,
-                            float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext){
-
+    private android.graphics.Rect makeGraphicsRect(Rect rect, float scaleBmpPxToCanvasPx) {
+        int left = Math.round(rect.x * scaleBmpPxToCanvasPx);
+        int top = Math.round(rect.y * scaleBmpPxToCanvasPx);
+        int right = left + Math.round(rect.width * scaleBmpPxToCanvasPx);
+        int bottom = top + Math.round(rect.height * scaleBmpPxToCanvasPx);
+        return new android.graphics.Rect(left, top, right, bottom);
     }
+<<<<<<< Updated upstream
     }
     
+=======
+
+        @Override
+public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight,
+        float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext)
+
+        {
+
+        Paint rectPaint = new Paint();
+        rectPaint.setColor(Color.RED);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(scaleCanvasDensity * 4);
+        canvas.drawRect(makeGraphicsRect(rect, scaleBmpPxToCanvasPx), rectPaint);
+        }
+        }
+>>>>>>> Stashed changes
