7eee3fa35ce69baf5be4f5fdf960d2fc805fbcdd
SiEclipzi <110932027+SiEclipzi@users.noreply.github.com>
2023-12-04 20:07:37 -0500

    global variables and pipeline for webcame vision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/var.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/var.java
@@ -1,5 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cRangeSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
 public class var {
 
     //heights for lift
@@ -12,5 +32,42 @@ public class var {
     public static double xAutoEnd = 0;
     public static double yAutoEnd = 0;
     public static double headingAutoEnd = 0;
+
+
+    public pipeline pipeline;
+    public HardwareMap hw = null;
+    public void init(HardwareMap thisHwMap) {
+        hw = thisHwMap;
+    }
+
+    public OpenCvWebcam webcam;
+    public var() {
+        //nothing goes in this- its just a way to call the program
+    }
+    public void initWebcam() {
+        // Create camera instance
+        int cameraMonitorViewId = hw.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hw.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hw.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+
+        // Open async and start streaming inside opened callback
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPRIGHT);
+
+                pipeline = new pipeline();
+                webcam.setPipeline(pipeline);
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+    }
+
 
 }
