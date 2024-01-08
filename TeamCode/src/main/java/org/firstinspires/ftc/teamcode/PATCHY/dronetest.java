b819e9b98c2a257d7b3128917be7205d929fcb55
KJScodes <104269100+KJScodes@users.noreply.github.com>
2024-01-08 18:49:13 -0500

    Create dronetest.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PATCHY/dronetest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.PATCHY;
+
+import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+//import com.qualcomm.robotcore.hardware.DigitalChannel;
+//import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Servo;
+//import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.RoadrunnerUtilStuff.drive.SampleMecanumDrive;
+//
+
+
+@TeleOp(name = "dronetest", group = "Linear Opmode")
+//@Disabled
+public class dronetest extends LinearOpMode {
+
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+
+    //ElapsedTime servoTime = new ElapsedTime();
+
+    }
+
